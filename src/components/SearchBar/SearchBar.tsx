9f8f6501f81dcfecfd0d99c5e6c5d26fe3945135
Ilka Garai <ilka.garai@tappointment.com>
2024-05-29 22:32:00 +0200

    MB-dev refactor: add useSearch hook

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,46 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { useQuery } from '../../hooks/useQuery';
+import { useSearch } from '../../hooks/useSearch';
 import * as S from './SearchBarStyles';
 
 export const SearchBar: React.FC = () => {
   const query = useQuery();
-  const location = useLocation();
-  const navigate = useNavigate();
 
   const initialSearchValue = query.get('search') ?? '';
 
-  const [searchValue, setSearchValue] = useState<string>(initialSearchValue);
-
-  useEffect(() => {
-    if (!searchValue || searchValue?.length < 4) return;
-
-    const timeOutId = setTimeout(() => {
-      const queryParams = new URLSearchParams(location.search);
-
-      queryParams.set('search', searchValue);
-      queryParams.set('page', '1');
-
-      navigate(`?${queryParams.toString()}`);
-    }, 500);
-
-    return () => clearTimeout(timeOutId);
-  }, [location.search, navigate, searchValue]);
-
-  const handleChange = useCallback(async (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    const value = event.target.value;
-
-    setSearchValue(value);
-  }, []);
-
-  const handleClick = useCallback(async () => {
-    const queryParams = new URLSearchParams(location.search);
-
-    queryParams.set('search', searchValue);
-    queryParams.set('page', '1');
-
-    navigate(`?${queryParams.toString()}`);
-  }, [location.search, navigate, searchValue]);
+  const { searchValue, handleChange, handleClick } = useSearch(initialSearchValue);
 
   return (
     <S.SearchBarContainer>
---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,45 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export const useSearch = (initialSearchValue: string = '') => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [searchValue, setSearchValue] = useState<string>(initialSearchValue);
+
+  useEffect(() => {
+    if (!searchValue || searchValue?.length < 4) return;
+
+    const timeOutId = setTimeout(() => {
+      const queryParams = new URLSearchParams(location.search);
+
+      queryParams.set('search', searchValue);
+      queryParams.set('page', '1');
+
+      navigate(`?${queryParams.toString()}`);
+    }, 500);
+
+    return () => clearTimeout(timeOutId);
+  }, [location.search, navigate, searchValue]);
+
+  const handleChange = useCallback(async (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setSearchValue(value);
+  }, []);
+
+  const handleClick = useCallback(async () => {
+    const queryParams = new URLSearchParams(location.search);
+
+    queryParams.set('search', searchValue);
+    queryParams.set('page', '1');
+
+    navigate(`?${queryParams.toString()}`);
+  }, [location.search, navigate, searchValue]);
+
+  return {
+    searchValue,
+    handleChange,
+    handleClick,
+  };
+};
