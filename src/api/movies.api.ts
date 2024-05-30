de625b4f99b1bfa875fba582eb0c2fad79544b75
Ilka Garai <ilka.garai@tappointment.com>
2024-05-30 18:45:40 +0200

    MB-dev feat: backend url config

--- src/api/movies.api.ts
+++ src/api/movies.api.ts
@@ -1,8 +1,9 @@
+import { config } from '../config/config';
 import { MoviesResponse } from '../types/movie.types';
 import { ApiResponse } from '../types/response.types';
 
 export const getMoviesBySearch = async (search: string, page: number): Promise<ApiResponse<MoviesResponse>> => {
-  const response = await fetch(`http://localhost:4001/api/movies?search=${search}&page=${page}`);
+  const response = await fetch(`${config.backendBaseUrl}/movies?search=${search}&page=${page}`);
 
   if (!response.ok) {
     throw new Error('Someting went wrong. ' + response.json());
---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  backendBaseUrl: process.env.BACKEND_BASE_URL ?? 'http://localhost:4001/api',
+};
